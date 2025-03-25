f08bfbc677f55832b49cbecdfac6e4e4c63c065f
LuisGlauser <luisfelipeglauser@hotmail.com>
2025-03-25 10:56:32 -0300

    add: Operadores e Tipos de Variaveis

---
+++ Tipos-Variaveis/src/Operadores.java
@@ -0,0 +1,43 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Operadores - 1
+        //String nomeCompleto = "LINGUAGEM" + " JAVA";
+        //System.out.println(nomeCompleto);
+
+        //Operadores - 2
+        //String concatencacao = "?";
+
+        //concatencacao = 1+1+1+"1";
+        //System.out.println(concatencacao);
+
+        //concatencacao = 1+"1"+1+"1";
+        //System.out.println(concatencacao);
+
+        //concatencacao = "1"+"1"+"1"+1;
+        //System.out.println(concatencacao);
+
+        //Operadores - 3
+        //int numero = 5;
+
+        //numero = -numero;
+        //System.out.println(numero);
+
+        //numero = numero*-1;
+        //System.out.println(numero);
+
+        //int numero = 5;
+        //x repetção
+
+        //numero = numero + 1;
+
+        //System.out.println(numero);
+
+        int a, b;
+        a = 10;
+        b = 20;
+        String resultado = a == b ? "Verdadeiro" : "Falso";
+        System.out.println(resultado);
+
+
+    }
+}
--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
+
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        String meuNome = "Luís Felipe Glauser Lucas";
+        double salario = 2500;
+
+        short numeroCurto = 1;
+        int numero = 100;
+        long numeroLongo = 1000000;
+
+        final double imutavel = 3.14;
+
     }
 }
